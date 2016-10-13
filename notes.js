d29c8e0e0fbf128d99c7bde43173d7e73526bec2
Matthew Erich <matthewerich1992@gmail.com>
2016-10-12 20:58:08 -0700

    sorting Algorithms Continued
    Implementing MergeSort and Stitch algorithms. Most usable as it
    requires the least amount of steps.

--- notes.js
+++ notes.js
@@ -101,6 +101,129 @@ factorial(5);//--> 120
 /////Sorting Algorithms/////
 //Bubble Sort//
 
+//[5,7,6,4]
+//[5,6,7,4]
+//[5,6,4,7]
+//[5,4,6,7]
+//[4,5,6,7]
+
+
+var bubbleSort = function(nums){
+	var swapped;
+	do {
+		 swapped = false;
+		for(var i =0;i<nums.length;i++){
+			if(nums[i] > nums[i+1]){
+				var temp = nums[i];
+				nums[i] = nums[i+1];
+				nums[i+1] = temp;
+				swapped = true;
+			}
+		}
+	} while(swapped);
+}
+
+bubbleSort([10,5,3,6,1,2]);
+
+
+//Insertion Sort
+// Very Usable -- > Arrays are almost sorted
+// Falls apart if Array is not sorted at all
+//[5,3,6]
+//[5] -- > then evaluates 3  and 6
+// will have O(n^2) as there are two nested loops
+
+
+//Two loops i and j --> i points to the first index
+//j is everything after i  and places each index
+// inner loop only does part of the arrau at a time, not the all of it
+// Big O cheatsheet .com --> Insertion and Bubble Sort
+
+function insertionSort(nums){
+	for(var i = 1;i<nums.length;i++){ // start at 1
+		for(var j = 0; j<i;j++){ // only look at the first part of the array
+			if(nums[i]<nums[j]){
+				var spliced = nums.splice(i,1); // removes from teh array, destructive
+				nums.splice(j,0, spliced[0]);// inserts at particular array spot
+			}
+		}
+	}
+}
+
+
+var a = [4,1,6, 8 , 7, 3, 42];
+
+insertionSort(a);
+ 
+ console.log(a);
+
+ //Call insertionSort much less than we would bubbleSort
+ // insertionSort does not have to do nearly as much work
+
+
+////Merge////
+// The most common sort -> array.sort uses merge sort underneath the hood
+//uses Recursion and is very consistent
+// takes a bigger list and breaks it down into smaller lists
+// the Big O is n log (n);
+
+
+
+function mergeSort(nums){
+	if(nums.length<2){ //if length of one it is already sorted
+		return nums;
+	}
+	var length = nums.length;
+	var middle = Math.floor(length/2); // will always round down and give smallest number
+	var left = nums.slice(0,middle);
+	var right = nums.slice(middle, length);
+	// Left array and a right array that are split.
+
+
+	return stitch(mergeSort(left), mergeSort(right));
+}
+
+// now right the stitch function
+
+var stitch = function(left, right){
+	var results = [];
+
+	while(left.length && right.length){
+		if(left[0]<= right[0]){
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+
+	}
+	while (left.length) {
+		results.push(left.shift());
+	}
+	while(right.length){
+		results.push(right.shift());
+	}
+
+	return results;
+};
+
+
+/////Median Values/////
+// Reddit Interview Question
+// Two sorted arrays, Get the median between the two arrays//
+
+//[1,5,8,9]
+//[2,3,7,10]
+//[1,2,3,4,7,8,9,10];
+
+
+
+///Quick Sort///
+// Similar to mergeSort
+//[5,7,4,9,6]
+// Choose a Pivot --> choose the last number in the array
+//[5,4] lower than six -- [7,9] greater than 6-- this continues to break down
+//[4,5,6,7,9]; All hinges upon choosing a pivot point
+
 
 
 
